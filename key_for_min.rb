74f3f31766210ebbfcb33a078a1e616a6f8e64c8
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-06 22:28:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,21 +4,22 @@
 
 
 def key_for_min_value(name_hash)
-  values = []
-  min = []
-  i = 0 
-  if name_hash == {}
-    return nil 
-  else 
-    while i < name_hash.length do
-      values << name_hash[i] 
-      count = 1 
-      while count < name_hash.length do
-        if values[i] < values[count]
-          min = values[i]
-          count += 1 
+  i = 0
+  vals = []
+  
+  while i < name_hash.count do
+    vals << name_hash.values[i]
+    if vals.count > 0
+      minimum = vals[0]
+      count = 1
+      while count < vals.count do
+        if vals[count] < minimum
+          minimum = vals[count]
         end
-      end 
-      i += 1 
+        count +=1 
+      end
     end
+    i += 1
   end
+  minimum
+end
